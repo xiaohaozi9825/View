47157c37fb9f2367cbba60438cc670a30bf2bb08
xiaohaozi9825 <1824465168@qq.com>
2021-05-21 16:11:22 +0800

    修改了子控件尺寸发生变化不能重新开启动画的bug 增加 AutosScrollView2 控件，改控件会拿到滚动视图的 bitmap，然后拼在一起滚动，这样就不会有一大片空白区域了

--- View/src/main/java/pw/xiaohaozi/view/AutoScrollView2.java
+++ View/src/main/java/pw/xiaohaozi/view/AutoScrollView2.java
@@ -2,16 +2,24 @@ package pw.xiaohaozi.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import pw.xiaohaozi.view.AutoScrollView.Attribute;
+import pw.xiaohaozi.view.AutoScrollView.Direction;
+import pw.xiaohaozi.view.AutoScrollView.ScrollCallBack;
 import pw.xiaohaozi.view.utils.Size;
 
 /**
@@ -25,7 +33,7 @@ import pw.xiaohaozi.view.utils.Size;
  * 1、如果想要子视图尺寸发生变化，则重新滚动，则需要将子控件的宽高设置成包裹内容
  */
 
-public class AutoScrollView extends FrameLayout {
+public class AutoScrollView2 extends LinearLayout {
     private static final String TAG = "AutoScrollView";
     /**
      * 慢速滚动
@@ -51,15 +59,15 @@ public class AutoScrollView extends FrameLayout {
     private ScrollCallBack mScrollCallBack;
     private boolean isScroll = false;
 
-    public AutoScrollView(@NonNull Context context) {
+    public AutoScrollView2(@NonNull Context context) {
         this(context, null);
     }
 
-    public AutoScrollView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public AutoScrollView2(@NonNull Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public AutoScrollView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public AutoScrollView2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         if (attrs != null) {
             //1、获取 TypedArray
@@ -111,18 +119,12 @@ public class AutoScrollView extends FrameLayout {
 //        init();
 //    }
     private void init() {
+        setOrientation(mDirection == Direction.VERTICAL ? VERTICAL : HORIZONTAL);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        Log.i(TAG, "onLayout: ");
-        super.onLayout(changed, left, top, right, bottom);
-        start();
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Log.i(TAG, "onMeasure: ");
         stop();
         // MeasureSpec.AT_MOST; 至多模式, 控件有多大显示多大, wrap_content
         // MeasureSpec.EXACTLY; 确定模式, 类似宽高写死成dip, match_parent
@@ -138,6 +140,7 @@ public class AutoScrollView extends FrameLayout {
         int mPaddingBottom = getPaddingBottom();
         //子控件尺寸测量
         View child = getChildAt(0);
+
         LayoutParams childLayoutParams = (LayoutParams) child.getLayoutParams();
         int childWidth = width - mPaddingLeft - mPaddingRight - childLayoutParams.leftMargin - childLayoutParams.rightMargin;
         int childHeight = height - mPaddingTop - mPaddingBottom - childLayoutParams.topMargin - childLayoutParams.bottomMargin;
@@ -148,13 +151,14 @@ public class AutoScrollView extends FrameLayout {
             childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.UNSPECIFIED);
             childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(childHeight, heightMode);
         } else {
-            //因为只能容纳一个空间，所以会填充宽度
+            //因为只能容纳一个控件，所以会填充宽度
             childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidth, widthMode);
             childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.UNSPECIFIED);
         }
         child.measure(childWidthMeasureSpec, childHeightMeasureSpec);//测量某一个子控件，以上三个方法都调用了该方法
+        getChildAt(1).measure(childWidthMeasureSpec, childHeightMeasureSpec);
 
-        //本空间尺寸测量
+        //本控件尺寸测量
         if (heightMode == MeasureSpec.AT_MOST) {
             if (mDirection == Direction.HORIZONTAL) {
                 height = child.getMeasuredHeight() + mPaddingTop + mPaddingBottom + childLayoutParams.topMargin + childLayoutParams.bottomMargin;
@@ -170,16 +174,31 @@ public class AutoScrollView extends FrameLayout {
 //                || mChildOldSize.getHeight() != mChildSize.getHeight()) {
 //            if (mWidth > 0 && mHeight > 0) reStart();
 //        }
-//
+//        if (mWidth > 0 && mHeight > 0)
+//        reStart();
         mChildOldSize = mChildSize;
     }
 
     @Override
     protected void onFinishInflate() {
-        if (getChildCount() > 1) {
+        if (getChildCount() > 2) {
             throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
         }
         super.onFinishInflate();
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        Log.i(TAG, "onLayout: " + changed);
+
+        View child = getChildAt(0);
+
+        ImageView imageView = (ImageView) getChildAt(1);
+        Bitmap toBitmap = toBitmap(child);
+        if (toBitmap != null) imageView.setImageBitmap(toBitmap);
+        reStart();
     }
 
     @Override
@@ -190,33 +209,53 @@ public class AutoScrollView extends FrameLayout {
 //        start();
     }
 
+    private Bitmap toBitmap(View v) {
+        int width = v.getWidth();
+        int height = v.getHeight();
+        if (width <= 0 || height <= 0) return null;
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.TRANSPARENT);
+        v.draw(canvas);
+        return bitmap;
+    }
+
     private void start() {
         Log.i(TAG, "start: ");
         if (getChildCount() < 1) return;
         final View child = getChildAt(0);
+        final ImageView imageView = (ImageView) getChildAt(1);
+
         int s;//位移（单位：像素）
 
         int childMeasuredWidth = child.getMeasuredWidth();
         int childMeasuredHeight = child.getMeasuredHeight();
 
         if (mDirection == Direction.HORIZONTAL) {
-            if (mAttribute == Attribute.EXCEED && childMeasuredWidth <= mWidth) return;
+            if (mAttribute == Attribute.EXCEED && childMeasuredWidth <= mWidth) {
+                if (imageView.getVisibility() != GONE) imageView.setVisibility(GONE);
+                return;
+            }
             s = childMeasuredWidth + mWidth;
             //fromXDelta     起始点X轴坐标，可以是数值、百分数、百分数p 三种样式，同scale
             //fromYDelta    起始点Y轴从标，可以是数值、百分数、百分数p 三种样式
             //toXDelta         结束点X轴坐标
             //toYDelta        结束点Y轴坐标
-            mTranslateAnimation = new TranslateAnimation(mWidth, -childMeasuredWidth, 0, 0);
+            mTranslateAnimation = new TranslateAnimation(0, -childMeasuredWidth, 0, 0);
+//            mTranslateAnimation1 = new TranslateAnimation(mWidth, -childMeasuredWidth, 0, 0);
         } else {
-            if (mAttribute == Attribute.EXCEED && childMeasuredHeight <= mHeight) return;
-
+            if (mAttribute == Attribute.EXCEED && childMeasuredHeight <= mHeight) {
+                if (imageView.getVisibility() != GONE) imageView.setVisibility(GONE);
+                return;
+            }
             s = childMeasuredHeight + mHeight;
-            mTranslateAnimation = new TranslateAnimation(0, 0, mHeight, -childMeasuredHeight);
+            mTranslateAnimation = new TranslateAnimation(0, 0, 0, -childMeasuredHeight);
+//            mTranslateAnimation = new TranslateAnimation(0, 0, mHeight, -childMeasuredHeight);
         }
 
         int t = s / mVelocity;//时间 （单位：毫秒）
         mTranslateAnimation.setDuration(t * 1000);
-
+        mTranslateAnimation.setInterpolator(new LinearInterpolator());
         //重复次数
         mTranslateAnimation.setRepeatCount(-1);
         mTranslateAnimation.setAnimationListener(new Animation.AnimationListener() {
@@ -228,6 +267,7 @@ public class AutoScrollView extends FrameLayout {
 
             @Override
             public void onAnimationEnd(Animation animation) {
+
                 if (mScrollCallBack != null) mScrollCallBack.onEnd(child);
                 isScroll = false;
             }
@@ -237,15 +277,21 @@ public class AutoScrollView extends FrameLayout {
                 if (mScrollCallBack != null) mScrollCallBack.onRepeat(child);
             }
         });
+
         child.startAnimation(mTranslateAnimation);
+        imageView.startAnimation(mTranslateAnimation);
+        if (imageView.getVisibility() != VISIBLE) imageView.setVisibility(VISIBLE);
     }
 
     private void stop() {
         Log.i(TAG, "stop: ");
         if (getChildCount() < 1) return;
-        final View child = getChildAt(0);
+
         if (mTranslateAnimation != null) {
-            child.clearAnimation();
+            getChildAt(0).clearAnimation();
+            View imageView = getChildAt(1);
+            imageView.clearAnimation();
+            mTranslateAnimation = null;
         }
     }
 
@@ -307,73 +353,52 @@ public class AutoScrollView extends FrameLayout {
 
 
     /**************************************************/
-    @Override
-    public void addView(View child) {
-        if (getChildCount() > 0) {
-            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
-        }
-        super.addView(child);
-    }
+//    @Override
+//    public void addView(View child) {
+//        if (getChildCount() > 0) {
+//            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
+//        }
+//
+//        super.addView(child);
+//
+//    }
 
-    @Override
-    public void addView(View child, int index) {
-        if (getChildCount() > 0) {
-            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
-        }
+//    @Override
+//    public void addView(View child, int index) {
+//        if (getChildCount() > 0) {
+//            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
+//        }
+//
+//        super.addView(child, index);
+//    }
 
-        super.addView(child, index);
-    }
-
-    @Override
-    public void addView(View child, ViewGroup.LayoutParams params) {
-        if (getChildCount() > 0) {
-            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
-        }
-
-        super.addView(child, params);
-    }
-
+//    @Override
+//    public void addView(View child, ViewGroup.LayoutParams params) {
+//        if (getChildCount() > 0) {
+//            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
+//        }
+//        Log.i(TAG, "addView: params");
+//        super.addView(child, params);
+//    }
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
-        if (getChildCount() > 0) {
+
+//        if (getChildCount() > 0) {
+//            throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
+//        }
+
+        int childCount = getChildCount();
+        if (childCount == 0) {
+            super.addView(child, 0, params);
+            ImageView imageView = new ImageView(getContext());
+            super.addView(imageView, 1, params);
+//            addView(imageView);
+        } else if (childCount == 1) {
+//            super.addView(child, index, params);
+        } else if (childCount > 1) {
             throw new IllegalStateException("AutoScrollView 只能拥有一个子控件");
         }
 
-        super.addView(child, index, params);
     }
 
-    /******************************************************/
-    public interface ScrollCallBack {
-        void onStart(View child);
-
-        void onEnd(View child);
-
-        void onRepeat(View child);
-
-    }
-
-    public enum Direction {
-
-        /**
-         * 水平滚动
-         */
-        HORIZONTAL,
-        /**
-         * 垂直滚动
-         */
-        VERTICAL
-
-    }
-
-    public enum Attribute {
-        /**
-         * 超出范围滚动，只有当子控件尺寸超出了父控件，才会滚动
-         */
-        EXCEED,
-        /**
-         * 永远滚动，不管子控件尺寸如何，都会滚动；
-         */
-        ALWAYS
-
-    }
 }
